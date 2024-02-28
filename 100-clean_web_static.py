671161b7f056ea7a3eb66e5799c8f8c9f2ba88e2
Musawenkosistar <musawenkosi2504@gmail.com>
2024-02-28 08:10:07 -0800

    AirBnB_clone_v2

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,7 +3,7 @@
 import os
 from fabric.api import *
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["54.198.55.96", "18.204.13.64"]
 
 
 def do_clean(number=0):
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["54.198.55.96", "18.204.13.64"]
 
 
 def do_pack():
