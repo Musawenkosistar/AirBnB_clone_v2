8cf872704ac6d5440965b01ae14f70053e8faed5
Musawenkosistar <musawenkosi2504@gmail.com>
2024-08-21 15:04:57 -0700

    AirBnB_clone_v2

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,14 @@
--- Script to create a MySQL server with the database hbnb_dev_db.
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
